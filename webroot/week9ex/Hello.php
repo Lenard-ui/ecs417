0c736c61cf0a93bce2ee0653c8ed055a1288c959
Lenard K <kemlenny@gmail.com>
2021-03-23 10:01:09 +0000

    more updates

--- webroot/week9ex/Hello.php
+++ webroot/week9ex/Hello.php
@@ -1,5 +1,5 @@
 <?php 
-    define("uname","Kem",case_insensitive:true);
+    define("USER","Kem");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,10 +7,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo uname?>'s webpage</title>
+    <title><?php echo USER; ?>'s webpage</title>
 </head>
 <body>
     <h1>Hello World</h1>
-    I am <?php echo uname; ?>'s webpage
+    I am <?php echo USER; ?>'s webpage
 </body>
 </html>
