51d83e5999e90f13b5376fa2d064f5af7b59c84d
Lenard K <kemlenny@gmail.com>
2021-04-11 17:27:38 +0100

    minor updates to databaseHandlers

--- webroot/courseworklvl2/includes/dataStream.php
+++ webroot/courseworklvl2/includes/dataStream.php
@@ -6,7 +6,7 @@ $dbuser = getenv("DATABASE_USER");
 $dbpwd = getenv("DATABASE_PASSWORD");
 $dbname = getenv("DATABASE_NAME");
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 
 if (!$conn) {
   die("Connection faild abort mission: ".mysqli_connect_error());
